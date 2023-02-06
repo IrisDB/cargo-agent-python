8e2b35409bea0d561f9c370f29260eae24a5bbba
Clemens Hahn <clemens@couchbits.com>
2023-02-06 10:45:29 +0100

    introduce unit tests

---
+++ test/analyzer/test_moving_pandas_analyzer.py
@@ -0,0 +1,53 @@
+import os.path
+from unittest import TestCase
+from src.analyzer.moving_pandas_analyzer import MovingPandasAnalyzer
+from test.config.definitions import ROOT_DIR
+
+
+class TestMovingPandasAnalyzer(TestCase):
+
+    def setUp(self) -> None:
+        self.sut = MovingPandasAnalyzer()
+
+    def test_it_should_analyze_input1(self):
+        # execute
+        actual = self.sut.analyze(path=self.__test_file("input1.pickle"))
+
+        # verify
+        self.assertEqual(actual['animals_total_number'], 3)
+        self.assertEqual(actual['animal_names'], ['Nemo blue', 'Nils yellow', 'Paula white'])
+        self.assertEqual(actual['taxa'], ['Anser anser'])
+        self.assertEqual(actual['track_names'], ['Nemo blue', 'Nils yellow', 'Paula white'])
+
+    def test_it_should_analyze_input2(self):
+        # execute
+        actual = self.sut.analyze(path=self.__test_file("input2.pickle"))
+
+        # verify
+        self.assertEqual(actual['animals_total_number'], 3)
+        self.assertEqual(actual['animal_names'], [742, 746, 749])
+        self.assertEqual(actual['taxa'], ['Anser albifrons'])
+        self.assertEqual(actual['track_names'], [742, 746, 749])
+
+    def test_it_should_analyze_input3(self):
+        # execute
+        actual = self.sut.analyze(path=self.__test_file("input3.pickle"))
+
+        # verify
+        self.assertEqual(actual['animals_total_number'], 1)
+        self.assertEqual(actual['animal_names'], ['Prinzesschen'])
+        self.assertEqual(actual['taxa'], ['Ciconia ciconia'])
+        self.assertEqual(actual['track_names'], ['Prinzesschen'])
+
+    def test_it_should_analyze_input4(self):
+        # execute
+        actual = self.sut.analyze(path=self.__test_file("input4.pickle"))
+
+        # verify
+        self.assertEqual(actual['animals_total_number'], 1)
+        self.assertEqual(actual['animal_names'], ['Goat-8810'])
+        self.assertEqual(actual['taxa'], ['Capra hircus'])
+        self.assertEqual(actual['track_names'], ['Goat-8810'])
+
+    def __test_file(self, file_name) -> str:
+        return os.path.join(ROOT_DIR, 'test', 'resources', file_name)
